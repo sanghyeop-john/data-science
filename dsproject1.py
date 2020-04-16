19a3c5b4f2592af29168684e7fd2185f07bb4046
Sanghyeop John Lee <sanghyeop-lee@uiowa.edu>
2020-04-16 03:33:03 -0700

    Initialized COCO

--- dsproject1.py
+++ dsproject1.py
@@ -26,6 +26,7 @@ create a file: touch [filename]
 check the status: git status
 track and stage a single file: git add [filename]
 track and stage all files: git add .
+undo git add before a commit: git reset or git reset [filename]
 commit with a message: git commit -m "description of commit"
 view the log: git log
 push changes: git push [remotename] [branchname]
